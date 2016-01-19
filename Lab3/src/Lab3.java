81b6cd0986cf6baf81f27c0d829eb9e44de33e25
J R Hayes <hayesj3@wit.edu>
2016-01-19 14:02:07 -0500

    Added Lab3 module and main class printing "hello world".

---
+++ Lab3/src/Lab3.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * Group Members: Christian Abate-Wong, Karen Camaso, Jacob Hayes
+ *
+ *
+ * @author Christian Abate-Wong
+ * @author Karen Camaso
+ * @author Jacob Hayes
+ *
+ * Created by hayesj3 on 1/19/2016.
+ */
+public class Lab3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
