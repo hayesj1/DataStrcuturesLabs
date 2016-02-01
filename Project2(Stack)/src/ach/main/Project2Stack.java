dcc7ba70eac540056c6269cd81485e79c401ff3d
Jacob <tigerbird1@verizon.net>
2016-02-01 16:15:09 -0500

    Finished Stack<T>

--- Project2(Stack)/src/ach/main/Project2Stack.java
+++ Project2(Stack)/src/ach/main/Project2Stack.java
@@ -3,7 +3,7 @@ package ach.main;
 /**
  * Created by hayesj3 on 2/1/2016.
  */
-public class Main {
+public class Project2Stack {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Optimus Calculator!");
