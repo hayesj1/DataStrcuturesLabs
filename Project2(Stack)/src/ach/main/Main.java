51768060c6a8d74a30bc39984dc62f79a53c646c
J R Hayes <hayesj3@wit.edu>
2016-02-01 15:22:04 -0500

    Initial Work on Project 2.

---
+++ Project2(Stack)/src/ach/main/Main.java
@@ -0,0 +1,11 @@
+package ach.main;
+
+/**
+ * Created by hayesj3 on 2/1/2016.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Optimus Calculator!");
+	}
+}
---
+++ Project2(Stack)/src/ach/stack/ArrayStack.java
@@ -0,0 +1,38 @@
+package ach.stack;
+
+/**
+ * Created by hayesj3 on 2/1/2016.
+ */
+public class ArrayStack<T> implements IStack<T> {
+
+	private T[] entries;
+	private int pos;
+
+	public ArrayStack() {
+
+	}
+	@Override
+	public void push(T entry) {
+
+	}
+
+	@Override
+	public T pop() {
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public void clear() {
+
+	}
+}
