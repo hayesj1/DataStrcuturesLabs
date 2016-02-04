9c0076319f2233ca59003f099f11fa6d5fe56a96
MalkyKaren <zioncleric@gmail.com>
2016-02-04 14:56:04 -0500

    started expression + operator

--- Lab3/src/ach/notation/PostfixEvaluator.java
+++ Lab3/src/ach/notation/PostfixEvaluator.java
@@ -1,8 +1,34 @@
 package ach.notation;
 
+
 /**
  * Created by camasok on 2/4/2016.
  */
 public class PostfixEvaluator
 {
+   static double sum=0.0;
+   char operator = '';
+   String expression = {}/* WHATEVER */;
+ public static double eval(String exp)
+ {
+     expression=exp;
+
+     for(int i = 0; i < expression.length(); i++)
+     {
+         operator = exp.charAt(i);
+
+         switch (operator)
+         {
+             case operator = '^' ;
+             case operator = '+' ;
+             case operator = '-' ;
+             case operator = '*' ;
+             case operator = '/' ;
+             case operator = '(' ;
+             case operator = ')' ;
+             case operator
+
+         }
+     }
+ }
 }
