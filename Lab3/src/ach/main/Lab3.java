0928f89c45e5b09611d37817957e732a1cbd41ce
Jacob <tigerbird1@verizon.net>
2016-02-04 14:55:33 -0500

    Started converter

--- Lab3/src/ach/main/Lab3.java
+++ Lab3/src/ach/main/Lab3.java
@@ -1,5 +1,9 @@
 package ach.main;
 
+import ach.notation.InfixToPostfix;
+
+import java.util.Scanner;
+
 /**
  *
  * Group Members: Christian Abate-Wong, Karen Camaso, Jacob Hayes
@@ -8,10 +12,13 @@ package ach.main;
  * @author Karen Camaso
  * @author Jacob Hayes
  *
- * Created by hayesj3 on 1/19/2016.
+ * Created by hayesj3 on 2/04/2016.
  */
 public class Lab3 {
 
+	String inFix;
+	String postFix;
+	double result;
     /**
      * Entry point of the program
      * @param args Command-line arguments
@@ -25,6 +32,15 @@ public class Lab3 {
      * in a real-world application of stack this would not be appropriate
      */
     public Lab3() {
-        System.out.println("Hello World!");
+        System.out.println("Enter an Infix Expression as a String: ");
+	    Scanner in = new Scanner(System.in);
+
+	    inFix = in.nextLine();
+	    System.out.println("You entered: " + inFix);
+	    postFix = InfixToPostfix.convert(inFix);
+	    System.out.println("The PostFix conversion of your expression is: " + postFix);
+	    //result = ;
+	    System.out.println("The Result of the Postfix : " + result);
+
     }
 }
--- Lab3/src/ach/notation/InfixToPostfix.java
+++ Lab3/src/ach/notation/InfixToPostfix.java
@@ -3,11 +3,18 @@ package ach.notation;
 import ach.stack.ArrayStack;
 
 /**
+ *
+ * Group Members: Christian Abate-Wong, Karen Camaso, Jacob Hayes
+ *
+ * @author Christian Abate-Wong
+ * @author Karen Camaso
+ * @author Jacob Hayes
+ *
  * Created by camasok on 2/4/2016.
  */
 public class InfixToPostfix
 {
-	public String convert(String infix) {
+	public static String convert(String infix) {
 		ArrayStack<Character> operatorStack = new ArrayStack<>();
 		StringBuilder postfix = new StringBuilder("");
 		int pos = 0;
@@ -16,38 +23,36 @@ public class InfixToPostfix
 			while(Character.isWhitespace(infix.charAt(pos++)));
 			char next = infix.charAt(pos);
 
-			if (Character.isAlphabetic(next)) {
-
-			}
-			/*switch (next)
-			{
-				case '^':
-					operatorStack.push(nextCharacter)
-					break
-				case '+' or '-' or '*' or '/' :
-					while (operatorStack is not empty and
-					precedence (nextCharacter <= precedence of operatorStack.peek())
-				{
-					Append operatorStack.peek() to postfix
-					operatorStack.pop()
+			if (Character.isAlphabetic(next) || Character.isDigit(next)) {
+				operatorStack.push(next);
+			} else {
+				switch (next) {
+					case '^':
+						operatorStack.push(next);
+						break;
+					case '+':
+					case '-':
+					case '*':
+					case '/':
+						while (!operatorStack.isEmpty() && (precedence(next) <= precedence(operatorStack.peek()))) {
+							postfix.append(operatorStack.pop());
+						}
+						operatorStack.push(next);
+						break;
+					case '(':
+						operatorStack.push(next);
+						break;
+					case ')': // the stack is not empty
+						char topOperator = operatorStack.pop();
+						while (topOperator != '(') {
+							postfix.append(topOperator);
+							topOperator = operatorStack.pop();
+						}
+						break;
+					default:
+						break; // ignore unexpected characters
 				}
-				operatorStack.push (nextCharacter)
-				break
-				case '(':
-					operatorStack.push (nextCharacter)
-					break;
-				8
-				case ')' : // the stack is not empty
-					topOperator = operatorStack.pop()
-					while (topOperator != '(')
-					{
-						append topOperator to postfix
-						topOperator = operatorStack.pop()
-					}
-					break;
-				default:
-					break // ignore unexpected characters
-			} */
+			}
 			pos++;
 		}
 		while (!operatorStack.isEmpty())
@@ -56,5 +61,15 @@ public class InfixToPostfix
 			postfix.append(topOperator);
 		}
 		return postfix.toString();
+	}
+
+	/**
+	 * Returns 1 for addition or subtraction, and 2 for multiplicatin and division, and 0 if op is not an operator
+	 * @param op the operator to test
+	 * @return 0,1,2 depending on the value of op
+	 */
+	private static int precedence(char op) {
+		return (
+				(op == '+') ? 1 : (op == '-') ? 1: (op == '*') ? 2 : (op == '/') ? 2 : 0);
 	}
 }
