713901ca37343ee2074faa74e10e14d6f61c19f1
Jacob <tigerbird1@verizon.net>
2016-02-09 15:19:25 -0500

    Implemented Test code for Project2

--- Project2(Stack)/src/ach/test/StackTester.java
+++ Project2(Stack)/src/ach/test/StackTester.java
@@ -1,9 +1,44 @@
 package ach.test;
 
+import ach.stack.ArrayStack;
+
 /**
  * Test code for Stack.
  * Created by hayesj3 on 2/2/2016.
  */
 public class StackTester {
 
+	/**
+	 * Testing driver for ArrayStack
+	 * @param args commandline args
+	 */
+	public static void main(String[] args) {
+		ArrayStack<String> strStack = new ArrayStack<>();
+		strStack.push("Food is cool");
+		System.out.println(strStack.peek());
+		strStack.clear();
+		System.out.println("isEmpty: " + strStack.isEmpty());
+		strStack = new ArrayStack<>(5);
+		strStack.push("Bacon");
+		strStack.push("Waffles");
+		strStack.push("Cheese Burger");
+		strStack.push("Bacon Cheese Burger");
+		strStack.push("Waffle-Bunned Bacon Cheese Burger");
+		outputStackContents(strStack);
+
+		ArrayStack<Integer> intStack = new ArrayStack<>(2);
+		intStack.push(1);
+		intStack.push(2);
+		intStack.push(3);
+		intStack.push(4);
+		intStack.push(5);
+		outputStackContents(intStack);
+		System.out.println("Testing completed successfully!");
+		}
+
+	private static void outputStackContents(ArrayStack strStack) {
+		while (!strStack.isEmpty()) {
+			System.out.println(strStack.pop());
+	}
+}
 }
