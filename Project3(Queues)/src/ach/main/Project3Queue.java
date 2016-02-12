58a7b56dc8e7dea635aba7afd7b2d2d320d0b66e
Jacob <tigerbird1@verizon.net>
2016-02-12 14:36:34 -0500

    Added base file stubs for Project3

---
+++ Project3(Queues)/src/ach/main/Project3Queue.java
@@ -0,0 +1,14 @@
+package ach.main;
+
+/**
+ * Group Members: Christian Abate-Wong, Karen Camaso, Jacob Hayes
+ *
+ * @author Christian Abate-Wong
+ * @author Karen Camaso
+ * @author Jacob Hayes
+ *
+ * Created by hayesj3 on 2/12/2016.
+ */
+public class Project3Queue {
+
+}
---
+++ Project3(Queues)/src/ach/queue/IQueue.java
@@ -0,0 +1,13 @@
+package ach.queue;
+
+/**
+ * Group Members: Christian Abate-Wong, Karen Camaso, Jacob Hayes
+ *
+ * @author Christian Abate-Wong
+ * @author Karen Camaso
+ * @author Jacob Hayes
+ *
+ * Created by hayesj3 on 2/12/2016.
+ */
+public interface IQueue<T> {
+}
