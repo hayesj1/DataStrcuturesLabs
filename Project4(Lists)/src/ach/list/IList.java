e39579688379f24ec6bd6ea6b819cd7ccba44d65
J R Hayes <hayesj3@wit.edu>
2016-03-16 14:38:19 -0400

    Fixed documentation

--- Project4(Lists)/src/ach/list/IList.java
+++ Project4(Lists)/src/ach/list/IList.java
@@ -11,20 +11,24 @@ package ach.list;
  */
 public interface IList<E> {
 
+	/**
+	 * Adds the provided entry to the end of the List
+	 * @param newEntry the new entry
+	 */
 	void add(E newEntry);
 
 	/**
-	 *
-	 * @param position
-	 * @param newEntry
+	 * Adds the provided entry to the List and at specified position
+	 * @param position the location in the list to place the new entry
+	 * @param newEntry the new entry
 	 * @throws IndexOutOfBoundsException if either givenPosition < 0 or
 	 * givenPosition > getLength()
 	 */
 	void add(int position, E newEntry);
 
 	/**
-	 *
-	 * @param position
+	 * Removes the entry at the specified position.
+	 * @param position the position of the entry that should be removed
 	 * @return the removed entry
 	 * @throws IndexOutOfBoundsException if either givenPosition < 0 or
 	 * givenPosition >= getLength()
@@ -32,17 +36,18 @@ public interface IList<E> {
 	E remove(int position);
 
 	/**
-	 *
-	 * @param position
+	 * Replaces the entry at <code>position</code> with <code>newEntry</code>
+	 * @param position the position of the entry to be replaced by <code>newEntry</code>
+	 * @param newEntry the newEntry
 	 * @return the replaced entry
 	 * @throws IndexOutOfBoundsException if either givenPosition < 0 or
 	 * givenPosition >= getLength()
 	 */
-	E replace (int position);
+	E replace (int position, E newEntry);
 
 	/**
-	 *
-	 * @param position
+	 * Gets the entry at the specified position
+	 * @param position the position of the entry to get
 	 * @return the entry at givenPosition
 	 * @throws IndexOutOfBoundsException if either givenPosition < 0 or
 	 * givenPosition >= getLength()
