1e39c4dbe984e1d40d64340cb78f3b649331aa53
Jacob <tigerbird1@verizon.net>
2016-03-16 15:07:10 -0400

    Added final Lab4 Stubs

---
+++ Lab4/src/ach/main/Lab4.java
@@ -0,0 +1,17 @@
+package ach.main;
+
+/**
+ * Group Members: Christian Abate-Wong, Karen Camaso, Jacob Hayes
+ *
+ * @author Christian Abate-Wong
+ * @author Karen Camaso
+ * @author Jacob Hayes
+ *         <p>
+ *         Created by hayesj3 on 3/16/2016.
+ */
+public class Lab4 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
