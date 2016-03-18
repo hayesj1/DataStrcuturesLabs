c8acc5212e2f3ef273a8c7f6329d74f0c2320de0
Jacob <tigerbird1@verizon.net>
2016-03-18 13:39:09 -0400

    Added instructional TODO

--- Lab4/src/ach/main/Lab4.java
+++ Lab4/src/ach/main/Lab4.java
@@ -12,6 +12,7 @@ package ach.main;
 public class Lab4 {
 
 	public static void main(String[] args) {
+		//TODO replace this with code that reads from the text file, adding each word to a DLList<String> and then tests all the methods
 		System.out.println("Hello World");
 	}
 }
