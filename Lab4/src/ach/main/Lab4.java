6507bfc2e9cff5206666b5f4005d8b16045762c9
MalkyKaren <zioncleric@gmail.com>
2016-03-21 12:35:07 -0400

    added methods to test program

--- Lab4/src/ach/main/Lab4.java
+++ Lab4/src/ach/main/Lab4.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * Group Members: Christian Abate-Wong, Karen Camaso, Jacob Hayes
  *
@@ -34,5 +36,39 @@ public class Lab4 {
 		}
 
 		//TODO add test code to demonstrate DLL.
+
+		System.out.println("Add test, adds burrito to end.");
+		testDLL.add("burrito");
+		System.out.println(Arrays.toString(testDLL.toArray()));
+
+		System.out.println("Add avacado after bob.");
+		testDLL.add(1, "avacado");
+		System.out.println(Arrays.toString(testDLL.toArray()));
+
+		System.out.println("Removes Zombie");
+		testDLL.remove(4);
+		System.out.println(Arrays.toString(testDLL.toArray()));
+
+		System.out.println("replace test, replaces bob with bill");
+		testDLL.replace(0, "bill");
+		System.out.println(Arrays.toString(testDLL.toArray()));
+
+		System.out.println("getEntry test, gets entry at position 5");
+		System.out.println(testDLL.getEntry(5));
+
+		System.out.println("Contains test, checks for burger");
+		System.out.println(testDLL.contains("burger"));
+
+		System.out.println(testDLL.isEmpty());
+		System.out.println(testDLL.getLength());
+
+		testDLL.clear();
+
+		System.out.println(testDLL.isEmpty());
+		System.out.println(testDLL.getLength());
+
+
+
+
 	}
 }
